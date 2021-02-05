b8b6216d65c97eaf2a05d55256db5b89838d082e
BrianTeam818 <le.danh.brian@gmail.com>
2021-02-05 18:24:24 -0500

    Fixed minor issues got the code to a workable state.

--- JackTankDrive/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ JackTankDrive/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,10 +1,10 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
+
 
 public class DriveTrain extends SubsystemBase {
   private TalonSRX talonLeft, talonRight;
