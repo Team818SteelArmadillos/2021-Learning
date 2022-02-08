5b633a93008649ec932e7a6ed9d6af2e3e7ba360
Jackstevenson13 <59897917+Jackstevenson13@users.noreply.github.com>
2022-02-07 19:46:53 -0500

    Revert "Newest Tank Drive"
    This reverts commit 06c63206508ab089c2a4ac0810222c4db9e3d657.

--- DriveSubsystem/src/main/java/frc/robot/commands/ExampleCommand.java
+++ DriveSubsystem/src/main/java/frc/robot/commands/ExampleCommand.java
@@ -4,34 +4,36 @@
 
 package frc.robot.commands;
 
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class DriveTrain extends CommandBase {
-  /** Creates a new DriveTrain. */
-  public DriveTrain() {
+/** An example command that uses an example subsystem. */
+public class ExampleCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final ExampleSubsystem m_subsystem;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ExampleCommand(ExampleSubsystem subsystem) {
+    m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    driveTrain.setLeftMotors(0); 
-    driveTrain.setRightMotors(0);
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    driveTrain.setLeftMotors(m_oi.getJoystickLeftY()); 
-    driveTrain.setRightMotors(m_oi.getJoystickRightY());
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    driveTrain.setLeftMotors(0); 
-    driveTrain.setRightMotors(0);
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
