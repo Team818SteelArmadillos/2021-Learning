108569313c3414c0e3ab3df220cdee980dfb973d
mthril <98191592+mthril@users.noreply.github.com>
2022-02-11 19:52:24 -0500

    cleaned auton
    clean

--- tankDrive/src/main/java/frc/robot/Robot.java
+++ tankDrive/src/main/java/frc/robot/Robot.java
@@ -7,7 +7,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.autonCommand;
 import frc.robot.commands.driveCommand;
+import frc.robot.subsystems.driveSubsystem;
+
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,7 +22,8 @@ import frc.robot.commands.driveCommand;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
+  public static driveSubsystem m_drivesubsystem;
+  public static autonCommand m_autonCommand;
   private RobotContainer m_robotContainer;
 
   /**
@@ -26,6 +32,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    m_drivesubsystem = new driveSubsystem();
+    m_autonCommand = new autonCommand();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
--- tankDrive/src/main/java/frc/robot/commands/autonCommand.java
+++ tankDrive/src/main/java/frc/robot/commands/autonCommand.java
@@ -4,13 +4,19 @@
 
 package frc.robot.commands;
 
+import java.sql.Time;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.OI;
+import frc.robot.Robot;
 import frc.robot.subsystems.driveSubsystem;
 
 public class autonCommand extends CommandBase {
+  public static double x = .25;
   autonCommand driveSub = new autonCommand();
   OI joyInput = new OI();
+  Timer autonTimer = new Timer();
   /** Creates a new driveCommand. */
   public autonCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -18,22 +24,28 @@ public class autonCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    Robot.m_drivesubsystem.setBothMotors(0);
+    autonTimer.start();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveSub.setRightMotors(joyInput.());
-    driveSub.setLeftMotors(joyInput.());
+    Robot.m_drivesubsystem.setRightMotors(x);
+    Robot.m_drivesubsystem.setLeftMotors(x);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+ Robot.m_drivesubsystem.setBothMotors(0);
+ autonTimer.stop();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+      return autonTimer.hasElapsed(3);
   }
 }
