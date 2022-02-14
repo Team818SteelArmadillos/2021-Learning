a76481241d582471c08c3a325780c4d7186f734d
Chase Watson <45761342+SinDanger@users.noreply.github.com>
2022-02-14 17:28:04 -0500

    Made changes to auton3secs
    Made a change to auton3secs timer code and made it more efficient

--- tankDrive/tankDrive/src/main/java/frc/robot/commands/auton3Secs.java
+++ tankDrive/tankDrive/src/main/java/frc/robot/commands/auton3Secs.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.motors;
 import frc.robot.subsystems.oi;
@@ -7,6 +8,7 @@ import frc.robot.subsystems.oi;
 public class auton3Secs extends CommandBase {
     motors MotorSubsystem = new motors();
     oi oiSubsystem = new oi();
+    Timer timer = new Timer();
       /** Creates a new motorSpeed. */
   public auton3Secs() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -15,24 +17,19 @@ public class auton3Secs extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    timer.start();
   }
     // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    long start = System.currentTimeMillis();
-    long end = start + 3*1000;
-    while (System.currentTimeMillis() < end) {
-        MotorSubsystem.SetLeftMotors(0.25);
-        MotorSubsystem.SetRightMotors(0.25); 
-    }
-    MotorSubsystem.SetLeftMotors(0);
-    MotorSubsystem.SetRightMotors(0);
+    MotorSubsystem.SetLeftMotors(0.25);
+    MotorSubsystem.SetRightMotors(0.25); 
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    timer.stop();
     MotorSubsystem.SetLeftMotors(0);
     MotorSubsystem.SetRightMotors(0);
   }
@@ -40,7 +37,7 @@ public class auton3Secs extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return timer.get() > 3;
   }
 
 }
