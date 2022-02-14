bf72b4b5e4b4223f4ec37e419d3d794a5a530856
mthril <98191592+mthril@users.noreply.github.com>
2022-02-14 18:07:33 -0500

    fixed turn
    fixed turn

--- tankDrive/src/main/java/frc/robot/Robot.java
+++ tankDrive/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.autonCommand;
+import frc.robot.commands.autonCommandturn;
 import frc.robot.commands.driveCommand;
 import frc.robot.subsystems.driveSubsystem;
 
@@ -24,6 +25,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   public static driveSubsystem m_drivesubsystem;
   public static autonCommand m_autonCommand;
+  public static autonCommandturn m_autonCommandturn;
   private RobotContainer m_robotContainer;
 
   /**
@@ -34,6 +36,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     m_drivesubsystem = new driveSubsystem();
     m_autonCommand = new autonCommand();
+    m_autonCommandturn = new autonCommandturn();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
@@ -65,8 +68,8 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-    
+    m_autonCommandturn = (autonCommandturn) m_robotContainer.getAutonomousCommand();
+
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
