89d77287dcad111bdf675d9a1c97806cefcb3940
Fadi Mandwee <839177@wcskids.net>
2022-02-16 17:25:58 -0500

    Update autoCMD.java

--- robotCode/src/main/java/frc/robot/commands/autoCMD.java
+++ robotCode/src/main/java/frc/robot/commands/autoCMD.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.driveSubsystem;
 
@@ -36,13 +37,11 @@ public class autoCMD extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    driveSubsystem.SetMotors(0, 0);
-    timer.stop();
+  public void execute() {
+    if(isFinished() == true){
+      driveSubsystem.SetMotors(0, 0);
+      timer.stop();
+    }
   }
 
   // Returns true when the command should end.
