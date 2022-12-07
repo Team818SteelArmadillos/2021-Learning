58b44374c8790d49099490613907a90ba2f74cc8
Jackstevenson13 <59897917+Jackstevenson13@users.noreply.github.com>
2022-12-06 19:33:51 -0500

    Update ShooterSubsystemTest.java

--- CafagnaTankDrivePractice/src/main/java/frc/robot/subsystems/ShooterSubsystemTest.java
+++ CafagnaTankDrivePractice/src/main/java/frc/robot/subsystems/ShooterSubsystemTest.java
@@ -5,12 +5,9 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
